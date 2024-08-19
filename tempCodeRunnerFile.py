01ae672f4d9d140b89c9a471fa18e95c503d0335
Imisi Bailey <imisibailey@gmail.com>
2024-08-19 16:57:59 +0100

    created a word replacement program

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+    # if word_to_replace in string:    
+    #     print(string.replace(word_to_replace,word_replacement))
+    # if word_to_replace not in string:
+    #     print("       unknown word", string, "does not include", word_to_replace)
+    
